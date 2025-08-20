04b8514496bf35fde8ea25de2d37d3b03ac4e466
penpharmacy <penpharmacypp@gmail.com>
2025-08-20 20:07:43 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'pill-calculator-cache-v1';
+const filesToCache = [
+'/',
+'/index.html',
+'/script.js',
+'/style.css',
+'/manifest.json'
+];
+
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+);
+});
+
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.match(event.request).then(response => response || fetch(event.request))
+);
+});
